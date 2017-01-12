e91e40cee4807aea28e10ffb7f9f22a96c8aa152
lijiarui <ruiruibupt@126.com>
2017-01-12 18:23:57 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Getting-Started-with-Wechaty---Live-Coding-Tutorial
+
+Wechaty is super easy to use, especially when you are using Docker.   
+This repo contains the code about the video tutorial.   
+
+[Click here](https://blog.wechaty.io/guide/2017/01/01/getting-started-wechaty.html "Click here") to watch the wechaty tutorial video
+
+# run  
+
+```sh
+git clone https://github.com/lijiarui/Getting-Started-with-Wechaty---Live-Coding-Tutorial.git
+docker run -ti --volume="$(pwd)":/bot --rm zixia/wechaty mybot.ts
+```
---
+++ mybot.ts
@@ -0,0 +1,59 @@
+import {Wechaty, Room} from 'wechaty'
+
+const bot = Wechaty.instance()
+
+bot
+.on('scan', (url, code)=>{
+    let loginUrl = url.replace('qrcode', 'l')
+    require('qrcode-terminal').generate(loginUrl)
+    console.log(url)
+})
+
+.on('login', user=>{
+    console.log(`${user} login`)
+})
+
+.on('friend', async function (contact, request){
+    if(request){
+        await request.accept()
+        console.log(`Contact: ${contact.name()} send request ${request.hello}`)
+    }
+})
+
+.on('message', async function(m){
+    const contact = m.from()
+    const content = m.content()
+    const room = m.room()
+
+    if(room){
+        console.log(`Room: ${room.topic()} Contact: ${contact.name()} Content: ${content}`)
+    } else{
+        console.log(`Contact: ${contact.name()} Content: ${content}`)
+    }
+
+    if(m.self()){
+        return
+    }
+
+    if(/hello/.test(content)){
+        m.say("hello how are you")
+    }
+
+    if(/room/.test(content)){
+        let keyroom = await Room.find({topic: "test"})
+        if(keyroom){
+            await keyroom.add(contact)
+            await keyroom.say("welcome!", contact)
+        }
+    }
+
+    if(/out/.test(content)){
+        let keyroom = await Room.find({topic: "test"})
+        if(keyroom){
+            await keyroom.say("Remove from the room", contact)
+            await keyroom.del(contact)
+        }
+    }
+})
+
+.init()
