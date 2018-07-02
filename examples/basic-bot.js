4cd633588f154675f47146e31e00a7430a026f04
Huan LI <zixia@zixia.net>
2018-07-03 01:36:12 +0800

    make it simple

---
+++ examples/basic-bot.js
@@ -0,0 +1,41 @@
+const qrTerm  = require('qrcode-terminal')
+
+const { 
+  Wechaty, 
+  Room 
+}             = require('wechaty')
+
+const bot = new Wechaty()
+
+bot.on('scan',    onScan)
+bot.on('login',   onLogin)
+bot.on('logout',  onLogout)
+bot.on('message', onMessage)
+
+bot.start()
+.catch(console.error)
+
+function onScan (qrcode, status) {
+  qrTerm.generate(qrcode, { small: true })  // show qrcode on console
+
+  const qrcodeImageUrl = [
+    'https://api.qrserver.com/v1/create-qr-code/?data=',
+    encodeURIComponent(qrcode),
+    '&size=220x220&margin=20',
+  ].join('')
+
+  console.log(qrcodeImageUrl)
+}
+
+function onLogin (user) {
+  console.log(`${user} login`)
+}
+
+function onLogout(user) {
+  console.log(`${user} logout`)
+}
+
+
+async function onMessage (msg) {
+  console.log(msg.toString())
+}
--- examples/six-lines-bot.js
+++
@@ -1,6 +0,0 @@
-const { Wechaty } = require('wechaty') // import { Wechaty } from 'wechaty'
-Wechaty.instance() // Global Instance
-.on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\n${qrcode}`))
-.on('login',            user => console.log(`User ${user} logined`))
-.on('message',       message => console.log(`Message: ${message}`))
-.start()
