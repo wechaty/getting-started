9f245d270379a3139862e39e714ff92774888db8
Huan LI <zixia@zixia.net>
2018-07-10 09:43:20 +0800

    add travis ci

---
+++ tests/smoke-testing.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env ts-node
+
+import { Wechaty }    from 'wechaty'
+
+async function main() {
+  const bot = Wechaty.instance()
+  try {
+    const future = new Promise(r => bot.once('scan', r))
+    await bot.start()
+    await future
+    console.log(`Wechaty v${bot.version()} smoking test passed.`)
+  } catch (e) {
+    console.error(e)
+    // Error!
+    return 1
+  } finally {
+    await bot.stop()
+  }
+  return 0
+}
+
+main()
+.then(process.exit)
+.catch(e => {
+  console.error(e)
+  process.exit(1)
+})
