cdfcf136dd1d1581709bb7847ae3ab2d3793bffa
Huan LI <zixia@zixia.net>
2018-07-19 09:38:50 +0800

    enhance smoke testing

--- tests/smoke-testing.js
+++ tests/smoke-testing.js
@@ -3,18 +3,36 @@
 const { Wechaty } = require('wechaty')
 
 async function main() {
-  const bot = Wechaty.instance({ puppet: 'mock' })
+  const botList = [
+    new Wechaty({ puppet: 'mock' }),
+    new Wechaty({ puppet: 'puppeteer' }),
+    new Wechaty({ puppet: 'padchat', puppetOptions: { token: 'smoke-testing' } }),
+    new Wechaty({ puppet: 'wechat4u' }),
+  ]
   try {
-    const future = new Promise(r => bot.once('scan', r))
-    await bot.start()
-    await future
-    console.log(`Wechaty v${bot.version()} smoking test passed.`)
+    const futureList = botList.map(
+      bot => new Promise(r => 
+        bot.once('scan', r),
+      ),
+    )
+    for (const bot of botList) {
+      await bot.start()
+    }
+    await Promise.all(futureList)
+    console.log(`Wechaty v${Wechaty.VERSION} smoking test passed.`)
+    botList.forEach(bot => {
+      console.log(`Wechaty Puppet ${bot.puppet} v${bot.puppet.version()} smoking test passed.`)
+    })
   } catch (e) {
     console.error(e)
     // Error!
     return 1
   } finally {
-    await bot.stop()
+    await Promise.all(
+      botList.map(
+        bot => bot.stop(),
+      ),
+    )
   }
   return 0
 }
