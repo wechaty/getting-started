a1a7782d40e5eacf707973f0a3a3d65aecd31448
TbhT <TbhT@users.noreply.github.com>
2018-08-16 01:14:16 +0800

    better examples for friend-bot.js (#47)
    * better examples for friend-not.js
    
    * remove unnecessary code
    
    only auto accept when `hello==='ding'`
    
    * Update friend-bot.js

--- examples/advanced/friend-bot.js
+++ examples/advanced/friend-bot.js
@@ -90,6 +90,10 @@ bot
           logMsg = 'accepted automatically because verify messsage is "ding"'
           console.log('before accept')
           await friendship.accept()
+
+          // if want to send msg , you need to delay sometimes
+          await new Promise(r => setTimeout(r, 1000))
+          await friendship.contact().say('hello from Wechaty')
           console.log('after accept')
 
         } else {
