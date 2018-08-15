de113e776ce931139e5e88f3074d88710238d0c1
wangbinghua <binghua.wang@upchina.com>
2018-08-15 18:11:50 +0800

    better examples for friend-not.js

--- examples/advanced/friend-bot.js
+++ examples/advanced/friend-bot.js
@@ -90,10 +90,19 @@ bot
           logMsg = 'accepted automatically because verify messsage is "ding"'
           console.log('before accept')
           await friendship.accept()
+
+          // if want to send msg , you need to delay sometimes
+          await new Promise(r => setTimeout(r, 1000))
+          await friendship.contact().say('hello world')
           console.log('after accept')
 
         } else {
           logMsg = 'not auto accepted, because verify message is: ' + friendship.hello()
+          await friendship.accept()
+
+          // if want to send msg , you need to delay sometimes
+          await new Promise(r => setTimeout(r, 1000))
+          await friendship.contact().say('hello world')
         }
         break
 
