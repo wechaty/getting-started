6a53f5c1b4999998beae1b5fd209b9e8d53a18b1
Huan LI <zixia@zixia.net>
2019-07-01 15:44:53 +0800

    code clean

--- tests/smoke-testing.js
+++ tests/smoke-testing.js
@@ -2,31 +2,34 @@
 
 const { Wechaty } = require('wechaty')
 
-async function main() {
+async function main () {
   const botList = [
     new Wechaty({ puppet: 'wechaty-puppet-mock' }),
     new Wechaty({ puppet: 'wechaty-puppet-puppeteer' }),
     new Wechaty({ puppet: 'wechaty-puppet-wechat4u' }),
   ]
 
-  if (   !process.env.TRAVIS_PULL_REQUEST
+  if (!process.env.TRAVIS_PULL_REQUEST
       && !process.env.APPVEYOR_PULL_REQUEST_NUMBER
   ) {
+    console.info('This CI test is a direct push to master branch.')
     botList.push(
       new Wechaty({ puppet: 'wechaty-puppet-padchat' }),
       new Wechaty({ puppet: 'wechaty-puppet-padpro' }),
     )
+  } else {
+    console.info('This CI test was activitated from a Pull Request.')
   }
 
   try {
     for (const bot of botList) {
-      const future = new Promise(r => bot.once('scan', r))
+      const future = new Promise(resolve => bot.once('scan', resolve))
       await bot.start()
       await future
       await bot.stop()
-      console.log(`Puppet ${bot.puppet} v${bot.puppet.version()} smoke testing passed.`)
+      console.info(`Puppet ${bot.puppet} v${bot.puppet.version()} smoke testing passed.`)
     }
-    console.log(`Wechaty v${Wechaty.VERSION} smoke testing passed.`)
+    console.info(`Wechaty v${Wechaty.VERSION} smoke testing passed.`)
   } catch (e) {
     console.error(e)
     // Error!
@@ -36,8 +39,8 @@ async function main() {
 }
 
 main()
-.then(process.exit)
-.catch(e => {
-  console.error(e)
-  process.exit(1)
-})
+  .then(process.exit)
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
