639f82bf57bf52d47f8fbf001c5db53e5ad81097
Huan (李卓桓) <zixia@zixia.net>
2020-02-22 10:42:38 +0800

    add this doc

--- examples/professional/hot-import-bot/listeners/on-friend.js
+++ examples/professional/hot-import-bot/listeners/on-friend.js
@@ -17,6 +17,12 @@
  *
  */
 async function onFriend (contact, request) {
+  /**
+   * We can get the Wechaty bot instance from this:
+   *   `const wechaty = this`
+   * Or use `this` directly:
+   *   `console.info(this.userSelf())`
+   */
   if(request){
     let name = contact.name()
     // await request.accept()
