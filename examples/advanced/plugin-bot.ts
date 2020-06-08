547bb714f50fe01db25744f3e1569741ced98329
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-09 01:29:30 +0800

    link to issue (https://github.com/wechaty/wechaty/issues/1939)

--- examples/advanced/plugin-bot.ts
+++ examples/advanced/plugin-bot.ts
@@ -20,6 +20,11 @@
 import {
   Wechaty, Message,
 }               from 'wechaty'
+
+/**
+ * Wechaty Plugin Support with Kickout Example #1939
+ *  https://github.com/wechaty/wechaty/issues/1939
+ */
 import {
   QRCodeTerminal,
   EventLogger,
