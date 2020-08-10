43c641ca034865f3f293964842b19d8a12518994
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-10 23:26:46 +0800

    add gitpod example

---
+++ examples/third-party/gitpod/ding-dong-bot.ts
@@ -0,0 +1,72 @@
+/**
+ * Wechaty - WeChat Bot SDK for Personal Account, Powered by TypeScript, Docker, and 💖
+ *  - https://github.com/wechaty/wechaty
+ */
+import {
+  Contact,
+  Message,
+  ScanStatus,
+  Wechaty,
+  log,
+}               from 'wechaty'
+
+import { generate } from 'qrcode-terminal'
+
+function onScan (qrcode: string, status: ScanStatus) {
+  if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
+    generate(qrcode, { small: true })  // show qrcode on console
+
+    const qrcodeImageUrl = [
+      'https://wechaty.github.io/qrcode/',
+      encodeURIComponent(qrcode),
+    ].join('')
+
+    log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
+  } else {
+    log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
+  }
+}
+
+function onLogin (user: Contact) {
+  log.info('StarterBot', '%s login', user)
+}
+
+function onLogout (user: Contact) {
+  log.info('StarterBot', '%s logout', user)
+}
+
+async function onMessage (msg: Message) {
+  log.info('StarterBot', msg.toString())
+
+  if (msg.text() === 'ding') {
+    await msg.say('dong')
+  }
+}
+
+const bot = new Wechaty({
+  name: 'ding-dong-bot',
+  /**
+   * Specify a `puppet` for a specific protocol (Web/Pad/Mac/Windows, etc).
+   *
+   * You can use the following providers:
+   *  - wechaty-puppet-hostie
+   *  - wechaty-puppet-puppeteer
+   *  - wechaty-puppet-padplus
+   *  - etc.
+   *
+   * Learn more about Wechaty Puppet Providers at:
+   *  https://github.com/wechaty/wechaty-puppet/wiki/Directory
+   */
+
+  // puppet: 'wechaty-puppet-hostie',
+
+})
+
+bot.on('scan',    onScan)
+bot.on('login',   onLogin)
+bot.on('logout',  onLogout)
+bot.on('message', onMessage)
+
+bot.start()
+  .then(() => log.info('StarterBot', 'Starter Bot Started.'))
+  .catch(e => log.error('StarterBot', e))
