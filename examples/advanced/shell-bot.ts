fb8358c6363411b9f0121ff6db7664fe77b6c038
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-29 22:33:30 +0800

    add shell-bot

---
+++ examples/advanced/shell-bot.ts
@@ -0,0 +1,20 @@
+import repl from 'repl'
+
+import { Wechaty } from 'wechaty'
+
+async function main () {
+  const wechaty = new Wechaty({
+    name: 'shell-bot',
+  })
+
+  console.info('Starting...')
+
+  await wechaty.start()
+
+  const shell = repl.start('wechaty> ')
+  shell.context.wechaty = wechaty
+  shell.on('exit', () => wechaty.stop())
+}
+
+main()
+  .catch(console.error)
