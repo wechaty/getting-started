66641a4c1b3d3db8151d6fefa8158b1bcf037459
Huan <zixia@zixia.net>
2021-08-31 18:11:22 +0800

    clean ESM support

---
+++ tests/wechaty-puppet-mock.spec.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env node --no-warnings --loader ts-node/esm
+
+import { test }     from 'tstest'
+import { Wechaty }  from 'wechaty'
+
+import {
+  mock,
+  PuppetMock,
+}               from 'wechaty-puppet-mock'
+
+test('wechaty-puppet-service', async t => {
+  const mocker = new mock.Mocker()
+  mocker.use(mock.SimpleEnvironment())
+  const puppetMock = new PuppetMock({ mocker })
+
+  const bot = new Wechaty({
+    puppet: puppetMock,
+  })
+
+  const timer = setTimeout(() => {
+    console.error('Smoke testing timeout after 2 minutes.')
+    process.exit(1)
+  }, 120 * 1000)
+
+  const future = new Promise(resolve => bot.once('scan', resolve))
+  await bot.start()
+  await future
+  await bot.stop()
+
+  clearTimeout(timer)
+  t.pass(`Puppet ${bot.puppet} v${bot.puppet.version()} smoke testing passed.`)
+})
---
+++ tests/wechaty-puppet-service.spec.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node --no-warnings --loader ts-node/esm
+
+import { test }     from 'tstest'
+import { Wechaty }  from 'wechaty'
+
+test('wechaty-puppet-service', async t => {
+  const bot = new Wechaty({
+    puppet: 'wechaty-puppet-service',
+    /**
+     * Huan(202108): our puppet service token is no-TLS for now.
+     * FIXME: enable TLS in the future
+     */
+    puppetOptions: { tls: { disable: true } },
+  })
+
+  const timer = setTimeout(() => {
+    console.error('Smoke testing timeout after 2 minutes.')
+    process.exit(1)
+  }, 120 * 1000)
+
+  const future = new Promise(resolve => bot.once('scan', resolve))
+  await bot.start()
+  await future
+  await bot.stop()
+
+  clearTimeout(timer)
+  t.pass(`Puppet ${bot.puppet} v${bot.puppet.version()} smoke testing passed.`)
+})
