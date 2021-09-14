109b5fea22888beaf49ae3edb573d59aaaf6572b
Souvik Biswas <sbis1999@gmail.com>
2021-09-14 12:49:10 +0530

    Restore the previous ding dong bot file

--- examples/third-parties/codesandbox/wechaty-ding-dong/ding-dong-bot.ts
+++ examples/third-parties/codesandbox/wechaty-ding-dong/ding-dong-bot.ts
@@ -2,58 +2,73 @@
  * Wechaty - WeChat Bot SDK for Personal Account, Powered by TypeScript, Docker, and 💖
  *  - https://github.com/wechaty/wechaty
  */
-import { Contact, Message, ScanStatus, Wechaty, log } from "wechaty";
+ import {
+  Contact,
+  Message,
+  ScanStatus,
+  Wechaty,
+  log,
+}               from 'wechaty'
 
-import { generate } from "qrcode-terminal";
+import { generate } from 'qrcode-terminal'
 
-function onScan(qrcode: string, status: ScanStatus) {
+function onScan (qrcode: string, status: ScanStatus) {
   if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
-    generate(qrcode, { small: true }); // show qrcode on console
+    generate(qrcode, { small: true })  // show qrcode on console
 
     const qrcodeImageUrl = [
-      "https://wechaty.js.org/qrcode/",
-      encodeURIComponent(qrcode)
-    ].join("");
+      'https://wechaty.js.org/qrcode/',
+      encodeURIComponent(qrcode),
+    ].join('')
 
-    log.info(
-      "StarterBot",
-      "onScan: %s(%s) - %s",
-      ScanStatus[status],
-      status,
-      qrcodeImageUrl
-    );
+    log.info('StarterBot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
   } else {
-    log.info("StarterBot", "onScan: %s(%s)", ScanStatus[status], status);
+    log.info('StarterBot', 'onScan: %s(%s)', ScanStatus[status], status)
   }
 }
 
-function onLogin(user: Contact) {
-  log.info("StarterBot", "%s login", user);
+function onLogin (user: Contact) {
+  log.info('StarterBot', '%s login', user)
 }
 
-function onLogout(user: Contact) {
-  log.info("StarterBot", "%s logout", user);
+function onLogout (user: Contact) {
+  log.info('StarterBot', '%s logout', user)
 }
 
-async function onMessage(msg: Message) {
-  log.info("StarterBot", msg.toString());
+async function onMessage (msg: Message) {
+  log.info('StarterBot', msg.toString())
 
-  if (msg.text() === "ding") {
-    await msg.say("dong");
+  if (msg.text() === 'ding') {
+    await msg.say('dong')
   }
 }
 
 const bot = new Wechaty({
-  name: "ding-dong-bot",
-  puppet: "wechaty-puppet-wechat4u"
-});
+  name: 'ding-dong-bot',
+  /**
+   * Specify a `puppet` for a specific protocol (Web/Pad/Mac/Windows, etc).
+   *
+   * You can use the following providers:
+   *  - wechaty-puppet-hostie
+   *  - wechaty-puppet-wechat
+   *  - wechaty-puppet-padplus
+   *  - etc.
+   *
+   * Learn more about Wechaty Puppet Providers at:
+   *  https://github.com/wechaty/wechaty-puppet/wiki/Directory
+   */
 
-bot.on("scan", onScan);
-bot.on("login", onLogin);
-bot.on("logout", onLogout);
-bot.on("message", onMessage);
+  // puppet: 'wechaty-puppet-hostie',
+  puppet: 'wechaty-puppet-wechat4u',
 
-bot
-  .start()
-  .then(() => log.info("StarterBot", "Starter Bot Started."))
-  .catch((e) => log.error("StarterBot", e));
+})
+
+bot.on('scan',    onScan)
+bot.on('login',   onLogin)
+bot.on('logout',  onLogout)
+bot.on('message', onMessage)
+
+bot.start()
+  .then(() => log.info('StarterBot', 'Starter Bot Started.'))
+  .catch(e => log.error('StarterBot', e))
+  
